ba734d5d818963db940e70b1202f7a77c9220620
Sterling Baldwin <sterlingbaldwin@users.noreply.github.com>
2022-01-19 12:04:23 +0000

    some improvements

--- nabu/story.py
+++ nabu/story.py
@@ -1,7 +1,8 @@
+from typing import ChainMap
 import yaml
 from pathlib import Path
 from jinja2 import Template
-from weasyprint import HTML
+from weasyprint import HTML, CSS
 # from xhtml2pdf import pisa
 
 class Story():
@@ -12,36 +13,36 @@ class Story():
         self.story_name = story_name
     
     def render(self, outpath):
+        # first render the html
         with open(self.template_path, "r") as fp:
             story_template = Template(fp.read())
         
         with open(Path(self.story_path, "story.yaml")) as fp:
             story_data = yaml.load(fp, Loader=yaml.SafeLoader)
-        
-        
-        # import ipdb; ipdb.set_trace()
+
+        # replace the image paths with the full path
+        for chapter in story_data['chapters']:
+            for page in chapter['pages']:
+                if 'image' in page:
+                    page['image'] = Path(self.story_path, page['image'])
 
         story_html = story_template.render(
+            cover_image=Path(self.story_path, story_data["cover_image"]),
             title=story_data["title"],
             author_name=story_data["author_name"],
             author_contact=story_data["author_contact"],
-            pages=story_data["pages"])
+            chapters=story_data["chapters"])
         
-        # style_string = self.get_style()
+        # now render the css
+        css_path = Path("nabu", "styles", "default.css")
+        with open(css_path, "r") as fp:
+            css_template = Template(fp.read())
+        
+        story_css = css_template.render(
+            chapters=story_data["chapters"])
+        
+        # finally, write out the pdf
         h = HTML(string=story_html)
-        h.write_pdf(outpath, stylesheets=[Path("nabu", "styles", "default_working.css")])
-        # with open(outpath, 'w+b') as outstream:
-        #     pisa.CreatePDF(
-        #         story_html,
-        #         dest=outstream)
-        
-        # with open(str(outpath).replace('pdf', 'html'), 'w') as outstream:
-        #     outstream.write(story_html)
-    
-    # def get_style(self):
-    #     with open(Path("nabu", "styles", f"default_working.css")) as fp:
-    #         return fp.read()
+        c = CSS(string=story_css)
+        h.write_pdf(outpath, stylesheets=[c])
 
-
-    
-    
